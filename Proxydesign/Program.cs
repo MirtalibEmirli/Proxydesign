8693fec5ba940ebd1ed8a59ba0ebb5afae10eeb3
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2024-05-27 21:47:23 +0400

    Add project files.

---
+++ Proxydesign/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+public abstract class Subject
+{
+    public abstract void Request();
+}
+
+public class RealSubject : Subject
+{
+    public override void Request()
+    {
+        Console.WriteLine("Called RealSubject.Request()");
+    }
+}
+
+public class Proxy : Subject
+{
+    private RealSubject realSubject;
+    public override void Request()
+    {
+        if (realSubject == null)
+        {
+            realSubject = new RealSubject();
+        }
+        realSubject.Request();
+    }
+}
